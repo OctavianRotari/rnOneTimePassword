60b476ff9fc21b5fc6d19438d1df6a0cd461c3b9
Octavian <octavianrotari@gmail.com>
2018-01-24 10:48:00 +0000

    typo

--- functions/twilio.js
+++ functions/twilio.js
@@ -3,4 +3,4 @@ const twilio = require('twilio');
 const accountSid = 'AC2c6d94aa5f677bf344175f8e2726aa30';
 const authToken = '420a230044e5eec728bcb8888315c7c0';
 
-module.exports = new twilio.Twilio(accountSid, authToke);
+module.exports = new twilio.Twilio(accountSid, authToken);
